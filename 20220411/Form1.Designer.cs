6eec1bbc9386d67baf0df1d67b9689c8d2f786d8
user <user@DESKTOP-6JBEF5K>
2022-04-11 12:02:42 +0800

    UDP塗鴉牆

--- 20220411/Form1.Designer.cs
+++ 20220411/Form1.Designer.cs
@@ -29,13 +29,175 @@ namespace _20220411
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbl_Target_IP = new System.Windows.Forms.Label();
+            this.lbl_Target_Port = new System.Windows.Forms.Label();
+            this.lbl_Answer_Port = new System.Windows.Forms.Label();
+            this.tb_Target_IP = new System.Windows.Forms.TextBox();
+            this.tb_Target_Port = new System.Windows.Forms.TextBox();
+            this.tb_Answer_Port = new System.Windows.Forms.TextBox();
+            this.btn_Connect = new System.Windows.Forms.Button();
+            this.rdb_Red = new System.Windows.Forms.RadioButton();
+            this.rdb_Blue = new System.Windows.Forms.RadioButton();
+            this.rdb_Green = new System.Windows.Forms.RadioButton();
+            this.rdb_Black = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // lbl_Target_IP
+            // 
+            this.lbl_Target_IP.AutoSize = true;
+            this.lbl_Target_IP.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lbl_Target_IP.Location = new System.Drawing.Point(12, 12);
+            this.lbl_Target_IP.Name = "lbl_Target_IP";
+            this.lbl_Target_IP.Size = new System.Drawing.Size(57, 21);
+            this.lbl_Target_IP.TabIndex = 0;
+            this.lbl_Target_IP.Text = "目標IP";
+            // 
+            // lbl_Target_Port
+            // 
+            this.lbl_Target_Port.AutoSize = true;
+            this.lbl_Target_Port.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lbl_Target_Port.Location = new System.Drawing.Point(193, 12);
+            this.lbl_Target_Port.Name = "lbl_Target_Port";
+            this.lbl_Target_Port.Size = new System.Drawing.Size(74, 21);
+            this.lbl_Target_Port.TabIndex = 1;
+            this.lbl_Target_Port.Text = "目標Port";
+            // 
+            // lbl_Answer_Port
+            // 
+            this.lbl_Answer_Port.AutoSize = true;
+            this.lbl_Answer_Port.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lbl_Answer_Port.Location = new System.Drawing.Point(391, 12);
+            this.lbl_Answer_Port.Name = "lbl_Answer_Port";
+            this.lbl_Answer_Port.Size = new System.Drawing.Size(74, 21);
+            this.lbl_Answer_Port.TabIndex = 2;
+            this.lbl_Answer_Port.Text = "接聽Port";
+            // 
+            // tb_Target_IP
+            // 
+            this.tb_Target_IP.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.tb_Target_IP.Location = new System.Drawing.Point(75, 9);
+            this.tb_Target_IP.Name = "tb_Target_IP";
+            this.tb_Target_IP.Size = new System.Drawing.Size(100, 29);
+            this.tb_Target_IP.TabIndex = 3;
+            // 
+            // tb_Target_Port
+            // 
+            this.tb_Target_Port.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.tb_Target_Port.Location = new System.Drawing.Point(273, 9);
+            this.tb_Target_Port.Name = "tb_Target_Port";
+            this.tb_Target_Port.Size = new System.Drawing.Size(100, 29);
+            this.tb_Target_Port.TabIndex = 4;
+            // 
+            // tb_Answer_Port
+            // 
+            this.tb_Answer_Port.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.tb_Answer_Port.Location = new System.Drawing.Point(471, 9);
+            this.tb_Answer_Port.Name = "tb_Answer_Port";
+            this.tb_Answer_Port.Size = new System.Drawing.Size(100, 29);
+            this.tb_Answer_Port.TabIndex = 5;
+            // 
+            // btn_Connect
+            // 
+            this.btn_Connect.AutoSize = true;
+            this.btn_Connect.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn_Connect.Location = new System.Drawing.Point(595, 7);
+            this.btn_Connect.Name = "btn_Connect";
+            this.btn_Connect.Size = new System.Drawing.Size(75, 31);
+            this.btn_Connect.TabIndex = 6;
+            this.btn_Connect.Text = "連線";
+            this.btn_Connect.UseVisualStyleBackColor = true;
+            this.btn_Connect.Click += new System.EventHandler(this.btn_Connect_Click);
+            // 
+            // rdb_Red
+            // 
+            this.rdb_Red.AutoSize = true;
+            this.rdb_Red.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.rdb_Red.Location = new System.Drawing.Point(16, 67);
+            this.rdb_Red.Name = "rdb_Red";
+            this.rdb_Red.Size = new System.Drawing.Size(58, 25);
+            this.rdb_Red.TabIndex = 7;
+            this.rdb_Red.TabStop = true;
+            this.rdb_Red.Text = "Red";
+            this.rdb_Red.UseVisualStyleBackColor = true;
+            // 
+            // rdb_Blue
+            // 
+            this.rdb_Blue.AutoSize = true;
+            this.rdb_Blue.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.rdb_Blue.Location = new System.Drawing.Point(160, 67);
+            this.rdb_Blue.Name = "rdb_Blue";
+            this.rdb_Blue.Size = new System.Drawing.Size(61, 25);
+            this.rdb_Blue.TabIndex = 8;
+            this.rdb_Blue.TabStop = true;
+            this.rdb_Blue.Text = "Blue";
+            this.rdb_Blue.UseVisualStyleBackColor = true;
+            // 
+            // rdb_Green
+            // 
+            this.rdb_Green.AutoSize = true;
+            this.rdb_Green.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.rdb_Green.Location = new System.Drawing.Point(80, 67);
+            this.rdb_Green.Name = "rdb_Green";
+            this.rdb_Green.Size = new System.Drawing.Size(74, 25);
+            this.rdb_Green.TabIndex = 9;
+            this.rdb_Green.TabStop = true;
+            this.rdb_Green.Text = "Green";
+            this.rdb_Green.UseVisualStyleBackColor = true;
+            // 
+            // rdb_Black
+            // 
+            this.rdb_Black.AutoSize = true;
+            this.rdb_Black.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.rdb_Black.Location = new System.Drawing.Point(227, 67);
+            this.rdb_Black.Name = "rdb_Black";
+            this.rdb_Black.Size = new System.Drawing.Size(68, 25);
+            this.rdb_Black.TabIndex = 10;
+            this.rdb_Black.TabStop = true;
+            this.rdb_Black.Text = "Black";
+            this.rdb_Black.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(687, 450);
+            this.Controls.Add(this.rdb_Black);
+            this.Controls.Add(this.rdb_Green);
+            this.Controls.Add(this.rdb_Blue);
+            this.Controls.Add(this.rdb_Red);
+            this.Controls.Add(this.btn_Connect);
+            this.Controls.Add(this.tb_Answer_Port);
+            this.Controls.Add(this.tb_Target_Port);
+            this.Controls.Add(this.tb_Target_IP);
+            this.Controls.Add(this.lbl_Answer_Port);
+            this.Controls.Add(this.lbl_Target_Port);
+            this.Controls.Add(this.lbl_Target_IP);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "UDP塗鴉牆";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbl_Target_IP;
+        private System.Windows.Forms.Label lbl_Target_Port;
+        private System.Windows.Forms.Label lbl_Answer_Port;
+        private System.Windows.Forms.TextBox tb_Target_IP;
+        private System.Windows.Forms.TextBox tb_Target_Port;
+        private System.Windows.Forms.TextBox tb_Answer_Port;
+        private System.Windows.Forms.Button btn_Connect;
+        private System.Windows.Forms.RadioButton rdb_Red;
+        private System.Windows.Forms.RadioButton rdb_Blue;
+        private System.Windows.Forms.RadioButton rdb_Green;
+        private System.Windows.Forms.RadioButton rdb_Black;
     }
 }
 
--- 20220411/Form1.cs
+++ 20220411/Form1.cs
@@ -8,13 +8,164 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Net; //匯入網路通訊協定相關函數
+using System.Net.Sockets; //匯入網路插座功能函數
+using System.Threading; //匯入多執行緒功能函數
+using Microsoft.VisualBasic.PowerPacks; //匯入 VB 向量繪圖功能
+
 namespace _20220411
 {
     public partial class Form1 : Form
     {
+        UdpClient U; //宣告UDP通訊物件
+        Thread Th;   //宣告監聽用執行續
+
+        //繪圖相關變數宣告
+        ShapeContainer C; //畫布物件(本機繪圖用)
+        ShapeContainer D; //畫布物件(遠端繪圖用)
+        Point stP; //繪圖起點
+        string p; //筆畫動作字串
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        //表單載入
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text += " " + MyIP(); //顯示本機IP於標題列
+            C = new ShapeContainer(); //建立畫布(本機繪圖用)
+            this.Controls.Add(C); //加入畫布C 到 form1
+            D = new ShapeContainer(); //建立畫布(遠端繪圖用)
+            this.Controls.Add(D); //加入畫布D 到 form1
+        }
+
+        //找出本機IP
+        private string MyIP()
+        {
+            string hn = Dns.GetHostName(); //取得本機電腦名稱
+            IPAddress[] ip = Dns.GetHostEntry(hn).AddressList; //取得本機IP陣列(可能有多個)
+            foreach (IPAddress it in ip) //列舉各個IP
+            {
+                if (it.AddressFamily == AddressFamily.InterNetwork) //如果是IPv4格式
+                {
+                    return it.ToString(); //傳回此IP字串
+                }
+            }
+            return ""; //找不到合格IP，回傳空字串
+        }
+
+        //監聽副程序
+        private void Listen()
+        {
+            int Port = int.Parse(tb_Target_Port.Text); //設定監聽用的通訊埠
+            U = new UdpClient(Port); //建立UDP監聽物件
+
+            IPEndPoint EP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), Port); //建立監聽端點資訊
+            while (true) //持續監聽的無限迴圈→有訊息(True)就處理，無訊息就等待!
+            {
+                byte[] B = U.Receive(ref EP); //讀取網路訊息(Byte陣列)
+                string A = Encoding.Default.GetString(B); //翻譯B陣列為字串A
+                string[] z = A.Split('_'); //切割顏色與座標資訊
+                string[] Q = z[1].Split('/'); //切割座標點資訊
+                Point[] R = new Point[Q.Length]; //宣告座標點陣列
+
+                // 定義繪圖座標點陣列
+                for (int i = 0; i < Q.Length; i++)
+                {
+                    string[] K = Q[i].Split(','); //切割X與Y座標
+                    R[i].X = int.Parse(K[0]); //定義第i點X座標
+                    R[i].Y = int.Parse(K[1]); //定義第i點Y座標
+                }
+
+                //繪製連續線段
+                for (int i = 0; i < Q.Length - 1; i++)
+                {
+                    LineShape L = new LineShape(); //建立線段物件
+                    L.StartPoint = R[i]; //線段起點
+                    L.EndPoint = R[i + 1]; //線段終點
+                    switch (z[0]) //筆色
+                    {
+                        case "1": //紅筆
+                            L.BorderColor = Color.Red;
+                            break;
+                        case "2": //亮綠筆
+                            L.BorderColor = Color.Lime;
+                            break;
+                        case "3": //藍筆
+                            L.BorderColor = Color.Blue;
+                            break;
+                        case "4": //黑筆
+                            L.BorderColor = Color.Black;
+                            break;
+                    }
+                    L.Parent = D; //線段L加入畫布D (遠端使用者繪圖)
+                }
+            }
+        }
+
+        //啟動監聽按鈕程序
+        private void btn_Connect_Click(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false; //忽略跨執行緒操作的錯誤
+            Th = new Thread(Listen); //建立監聽執行緒，目標副程式→Listen
+            Th.Start(); //啟動監聽執行緒
+            btn_Connect.Enabled = false; //按鍵失效，不能(也不需要)重複開啟監聽
+        }
+
+        //本機端開始繪圖
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            stP = e.Location; //紀錄起點
+            p = stP.X.ToString() + "," + stP.Y.ToString(); //紀錄座標
+        }
+
+        //本機端繪圖中
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left) //左鍵按下時
+            {
+                LineShape L = new LineShape(); //建立線段物件
+                L.StartPoint = stP; //線段起點
+                L.EndPoint = e.Location; //線段終點
+                if(rdb_Red.Checked) { L.BorderColor = Color.Red; } //紅筆
+                if (rdb_Green.Checked) { L.BorderColor = Color.Lime; } //亮綠筆
+                if (rdb_Blue.Checked) { L.BorderColor = Color.Blue; } //藍筆
+                if (rdb_Black.Checked) { L.BorderColor = Color.Black; } //黑筆
+                L.Parent = C; //線段加入線段C
+                stP = e.Location; //終點變起點
+                p += "/" + stP.X.ToString() + "," + stP.Y.ToString(); //持續記錄座標
+            }
+        }
+
+        //送出繪圖動作
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            int Port = int.Parse(tb_Target_Port.Text); //設定發送的目標Port
+            UdpClient S = new UdpClient(tb_Target_IP.Text, Port); //建立UDP物件
+            if (rdb_Red.Checked) { p = "1_" + p; } //紅筆
+            if (rdb_Green.Checked) { p = "2_" + p; } //亮綠筆
+            if (rdb_Blue.Checked) { p = "3_" + p; } //藍筆
+            if (rdb_Black.Checked) { p = "4_" + p; } //黑筆
+            byte[] B = Encoding.Default.GetBytes(p); //翻譯p字串為B陣列
+            S.Send(B, B.Length); //發送資料
+            S.Close(); //關閉UDP物件
+        }
+
+        //關閉監聽執行續(如果有的話)
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                Th.Abort(); //關閉監聽執行續
+                U.Close();  //關閉監聽器
+            }
+            catch
+            {
+                //忽略錯誤，程式繼續執行
+            }
+        }
+
     }
 }
